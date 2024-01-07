7d969820569d0269f469c8704683b2e6b1bee845
Tuấn Trần <tuantransn2001@gmail.com>
2024-01-07 22:42:11 +0700

    first commit

---
+++ assertion-analyser.js
@@ -0,0 +1,94 @@
+function objParser(str, init) {
+  var openSym = ["[", "{", '"', "'", "("];
+  var closeSym = ["]", "}", '"', "'", ")"];
+  var type;
+  for (var i = init || 0; i < str.length; i++) {
+    type = openSym.indexOf(str[i]);
+    if (type !== -1) break;
+  }
+  if (type === -1) return null;
+  var open = openSym[type];
+  var close = closeSym[type];
+  var count = 1;
+  for (var k = i + 1; k < str.length; k++) {
+    if (open === '"' || open === "'") {
+      if (str[k] === close) count--;
+      if (str[k] === "\\") k++;
+    } else {
+      if (str[k] === open) count++;
+      if (str[k] === close) count--;
+    }
+    if (count === 0) break;
+  }
+  if (count !== 0) return null;
+  var obj = str.slice(i, k + 1);
+  return {
+    start: i,
+    end: k,
+    obj: obj,
+  };
+}
+
+function replacer(str) {
+  var obj;
+  var cnt = 0;
+  var data = [];
+  while ((obj = objParser(str))) {
+    data[cnt] = obj.obj;
+    str =
+      str.substring(0, obj.start) + "__#" + cnt++ + str.substring(obj.end + 1);
+  }
+  return {
+    str: str,
+    dictionary: data,
+  };
+}
+
+function splitter(str) {
+  var strObj = replacer(str);
+  var args = strObj.str.split(",");
+  args = args.map(function (a) {
+    var m = a.match(/__#(\d+)/);
+    while (m) {
+      a = a.replace(/__#(\d+)/, strObj.dictionary[m[1]]);
+      m = a.match(/__#(\d+)/);
+    }
+    return a.trim();
+  });
+  return args;
+}
+
+function assertionAnalyser(body) {
+  if (!body) return "invalid assertion";
+  var body = body.match(/(?:browser\s*\.\s*)?assert\s*\.\s*\w*\([\s\S]*\)/)[0];
+  var s = replacer(body);
+  var splittedAssertions = s.str.split("assert");
+  var assertions = splittedAssertions.slice(1);
+
+  var assertionBodies = [];
+  var methods = assertions.map(function (a, i) {
+    var m = a.match(/^\s*\.\s*(\w+)__#(\d+)/);
+    assertionBodies.push(parseInt(m[2]));
+    var pre = splittedAssertions[i].match(/browser\s*\.\s*/) ? "browser." : "";
+    return pre + m[1];
+  });
+  if (
+    methods.some(function (m) {
+      return !m;
+    })
+  )
+    return "invalid assertion";
+
+  var bodies = assertionBodies.map(function (b) {
+    return s.dictionary[b].slice(1, -1).trim();
+  });
+  assertions = methods.map(function (m, i) {
+    return {
+      method: m,
+      args: splitter(bodies[i]),
+    };
+  });
+  return assertions;
+}
+
+module.exports = assertionAnalyser;
---
+++ tests/1_unit-tests.js
@@ -0,0 +1,181 @@
+var chai = require("chai");
+var assert = chai.assert;
+
+var server = require("../server");
+
+var chaiHttp = require("chai-http");
+chai.use(chaiHttp);
+
+suite("Functional Tests", function () {
+  test("Asynchronous test #example", function (done) {
+    setTimeout(function () {
+      assert.isOk("Async test !!");
+      done();
+    }, 500);
+  });
+
+  suite("Integration tests with chai-http", function () {
+    suite('GET /hello?name=[name] => "hello [name]"', function () {
+      test("#example - ?name=John", function (done) {
+        chai
+          .request(server)
+          .get("/hello?name=John")
+          .end(function (err, res) {
+            assert.equal(res.status, 200, "response status should be 200");
+            assert.equal(
+              res.text,
+              "hello John",
+              'response should be "hello John"'
+            );
+            done();
+          });
+      });
+
+      test("Test GET /hello with no name", function (done) {
+        chai
+          .request(server)
+          .get("/hello")
+          .end(function (err, res) {
+            assert.equal(res.status, 200);
+            assert.equal(res.text, "hello Guest");
+            done();
+          });
+      });
+
+      test("Test GET /hello with your name", function (done) {
+        chai
+          .request(server)
+          .get("/hello?name=dev")
+          .end(function (err, res) {
+            assert.equal(res.status, 200);
+            assert.equal(res.text, "hello dev");
+            done();
+          });
+      });
+    });
+
+    suite("PUT /travellers", function () {
+      test('#example - responds with appropriate JSON data when sending {surname: "Polo"}', function (done) {
+        chai
+          .request(server)
+          .put("/travellers")
+          .send({ surname: "Polo" })
+          .end(function (err, res) {
+            assert.equal(res.status, 200, "response status should be 200");
+            assert.equal(
+              res.type,
+              "application/json",
+              "Response should be json"
+            );
+            assert.equal(
+              res.body.name,
+              "Marco",
+              'res.body.name should be "Marco"'
+            );
+            assert.equal(
+              res.body.surname,
+              "Polo",
+              'res.body.surname should be "Polo"'
+            );
+            done();
+          });
+      });
+
+      test('send {surname: "Colombo"}', function (done) {
+        chai
+          .request(server)
+          .put("/travellers")
+          .send({ surname: "Colombo" })
+          .end(function (err, res) {
+            assert.equal(res.status, 200, "response status should be 200");
+            assert.equal(
+              res.type,
+              "application/json",
+              "Response should be json"
+            );
+            assert.equal(
+              res.body.name,
+              "Cristoforo",
+              'res.body.name should be "Cristoforo"'
+            );
+            assert.equal(
+              res.body.surname,
+              "Colombo",
+              'res.body.surname should be "Colombo"'
+            );
+            done();
+          });
+      });
+
+      test('send {surname: "da Verrazzano"}', function (done) {
+        chai
+          .request(server)
+          .put("/travellers")
+          .send({ surname: "da Verrazzano" })
+          .end(function (err, res) {
+            assert.equal(res.status, 200, "response status should be 200");
+            assert.equal(
+              res.type,
+              "application/json",
+              "Response should be json"
+            );
+            assert.equal(
+              res.body.name,
+              "Giovanni",
+              'res.body.name should be "Giovanni"'
+            );
+            assert.equal(
+              res.body.surname,
+              "da Verrazzano",
+              'res.body.surname should be "da Verrazzano"'
+            );
+            done();
+          });
+      });
+    });
+  });
+
+  var Browser = require("zombie");
+
+  Browser.site = "https://calm-cocoa.glitch.me";
+
+  suite("e2e Testing with Zombie.js", function () {
+    const browser = new Browser();
+
+    suiteSetup(function (done) {
+      return browser.visit("/", done);
+    });
+
+    suite('"Famous Italian Explorers" form', function () {
+      test('submit "surname" : "Polo"', function (done) {
+        browser.fill("surname", "Polo").pressButton("submit", function () {
+          browser.assert.success();
+          browser.assert.text("span#name", "Marco");
+          browser.assert.text("span#surname", "Polo");
+          browser.assert.element("span#dates", 1);
+          done();
+        });
+      });
+
+      test('submit "surname" : "Colombo"', function (done) {
+        browser.fill("surname", "Colombo").pressButton("submit", function () {
+          browser.assert.success();
+          browser.assert.text("span#name", "Cristoforo");
+          browser.assert.text("span#surname", "Colombo");
+          browser.assert.element("span#dates", 1);
+          done();
+        });
+      });
+
+      test('submit "surname" : "Vespucci"', function (done) {
+        browser.fill("surname", "Vespucci").pressButton("submit", function () {
+          browser.assert.success();
+          browser.assert.text("span#name", "Amerigo");
+          browser.assert.text("span#surname", "Vespucci");
+          browser.assert.element("span#dates", 1);
+          done();
+        });
+      });
+    });
+  });
+});
